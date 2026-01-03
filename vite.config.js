93166a3b4a7995c864c25e904842a64697a5ca2d
Maatheus764 <agentmkbusiness@gmail.com>
2026-01-03 09:43:34 -0300

    Update allowedHosts in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ["kds-zevorise.dahead.easypanel.host"],
+    allowedHosts: ["evorise.com.br"],
   },
 });
