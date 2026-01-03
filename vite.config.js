bbbbbcf665f96bb42d12dbff06424bf305982ad8
evoriseevobd-wq <evorise.evobd@gmail.com>
2026-01-03 09:53:35 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    allowedHosts: ["evorise.com.br"],
-  },
-});
+  server: {
+    host: true,
+    preview: {
+      allowedHosts: ['evoriseai.com.br']
+    }
+  }
+})
